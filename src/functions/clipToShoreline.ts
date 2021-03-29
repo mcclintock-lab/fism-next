f58dcc886355aca22bd449889fc23cac990abe1b
Chad Burt <chad@underbluewaters.net>
2021-03-29 07:40:59 -0700

    Add shoreline clipping with variable buffer from shoreline

--- src/functions/clipToShoreline.ts
+++ src/functions/clipToShoreline.ts
@@ -3,18 +3,38 @@ import {
   PreprocessingHandler,
 } from "@seasketch/geoprocessing";
 import area from "@turf/area";
-import { Feature, BBox, Polygon } from "geojson";
-import shoreline from "../../data/src/shoreline-multi.json";
-import { diff } from "martinez-polygon-clipping";
+import { Feature, BBox, Polygon, MultiPolygon } from "geojson";
+import shoreline from "../../data/shore-multi-new.json";
+import { diff, intersection } from "martinez-polygon-clipping";
+import buffer from "@turf/buffer";
+
+const DEFAULT_BUFFER = 750;
+const BUFFER_ATTR = "BUFF_DIST";
 
 async function clipToShoreline(feature: Feature): Promise<Feature> {
+  console.time("clip");
   if (!isPolygon(feature)) {
     throw new ValidationError("Input must be a polygon");
   }
-  const clippedCoords = diff(
+  const bufferDistance =
+    feature.properties && feature.properties[BUFFER_ATTR]
+      ? parseInt(feature.properties[BUFFER_ATTR])
+      : DEFAULT_BUFFER;
+
+  // Create clipping mask
+  const buffered = buffer(
+    shoreline.features[0] as Feature<MultiPolygon>,
+    bufferDistance,
+    { units: "feet" }
+  );
+
+  let clippedCoords = diff(
     feature.geometry.coordinates,
     shoreline.features[0].geometry.coordinates
   );
+
+  clippedCoords = intersection(clippedCoords, buffered.geometry.coordinates);
+
   const clipped = {
     ...feature,
     geometry: {
@@ -32,8 +52,8 @@ async function clipToShoreline(feature: Feature): Promise<Feature> {
 export default new PreprocessingHandler(clipToShoreline, {
   title: "clipToShoreline",
   description: "Erases land features from the sketch",
-  timeout: 1,
-  memory: 256,
+  timeout: 30,
+  memory: 1024,
   requiresProperties: [],
 });
 
